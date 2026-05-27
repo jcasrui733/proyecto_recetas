87306b11be7d93c0bf24d24465bcacb6ed978f68
Juan Antonio Castro Ruiz <jcasrui733@fp.iesromerovargas.com>
2026-05-27 12:03:30 +0200

    cambios recera

--- recetas/admin.py
+++ recetas/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Categoria , Receta, Comentario
+# from .models import Categoria , Receta, Comentario
 
 
--- recetas/models.py
+++ recetas/models.py
@@ -1,25 +1,24 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 
-class Categoria(models.Model):
-    nombre = models.CharField(max_length=100)
-    descripcion = models.TextField(blank=True,null =True)
+# class Categoria(models.Model):
+#     nombre = models.CharField(max_length=100)
+#     descripcion = models.TextField(blank=True,null =True)
 
-    class Meta:
-        verbose_name = "Categoria"
-        verbose_name_plural = "Categorias"
+#     class Meta:
+#         verbose_name = "Categoria"
+#         verbose_name_plural = "Categorias"
 
-    def __str__(self):
-        return self.nombre
+#     def __str__(self):
+#         return self.nombre
 
 class Receta(models.Model):
     titulo = models.CharField(max_length = 100)
     ingredientes = models.TextField( blank=False,null=False)
     pasos_elaboracion = models.TextField(blank=False,null=False)
     tiempo_preparacion = models.PositiveIntegerField()
-    categoria = models.ForeignKey(Categoria, on_delete= models.CASCADE, related_name="categoria")
-    autor = models.ForeignKey(User, on_delete= models.CASCADE, related_name="autor")
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.CASCADE, related_name="recetas")
     fecha_creacion = models.DateTimeField(auto_now_add= True)
     
     class Meta:
@@ -32,17 +31,15 @@ class Receta(models.Model):
 
 
 
-class Comentario(models.Model):
-    contenido = models.TextField(blank=True, null=False)
-    autor = models.ForeignKey(User,on_delete=models.CASCADE, related_name="autor")
-    reseta_asociada = models.ForeignKey(Receta, on_delete=models.CASCADE, related_name="recetas")
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
+# class Comentario(models.Model):
+#     contenido = models.TextField(blank=True, null=False)
+#     autor = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name="comentarios")
+#     # reseta_asociada = models.ForeignKey(Receta, on_delete=models.CASCADE, related_name="recetas")
+#     fecha_creacion = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        ordering = ["-fecha_creacion"]
+#     class Meta:
+#         ordering = ["-fecha_creacion"]
 
-    def __str__(self):
-        return f" Comentario de {self.autor.username} en {self.receta.titulo}"
+#     def __str__(self):
+#         return f" Comentario de {self.autor.username} en {self.receta.titulo}"
     
-
-
