7a00fe2ef8b99aac105b1e0006995ddcd7fdce71
Juan Antonio Castro Ruiz <jcasrui733@fp.iesromerovargas.com>
2026-05-27 11:19:29 +0200

    cambio modelos

--- recetas/admin.py
+++ recetas/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Categoria , Receta, Comentario
 
-# Register your models here.
+
--- recetas/models.py
+++ recetas/models.py
@@ -1,12 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True,null =True)
+
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+
+    def __str__(self):
+        return self.nombre
 
 class Receta(models.Model):
     titulo = models.CharField(max_length = 100)
     ingredientes = models.TextField( blank=False,null=False)
     pasos_elaboracion = models.TextField(blank=False,null=False)
     tiempo_preparacion = models.PositiveIntegerField()
-    Categoria = models.CharField(max_length=100,)
-    Autor = models.CharField(max_length=100)
-    Fecha_creacion = models.DateTimeField(auto_now_add= True)
+    categoria = models.ForeignKey(Categoria, on_delete= models.CASCADE, related_name="categoria")
+    autor = models.ForeignKey(User, on_delete= models.CASCADE, related_name="autor")
+    fecha_creacion = models.DateTimeField(auto_now_add= True)
+    
+    class Meta:
+        ordering = ["-fecha_creacion"]
+
+    def __str__(self):
+        return self.titulo
+
+
+
+
+
+class Comentario(models.Model):
+    contenido = models.TextField(blank=True, null=False)
+    autor = models.ForeignKey(User,on_delete=models.CASCADE, related_name="autor")
+    reseta_asociada = models.ForeignKey(Receta, on_delete=models.CASCADE, related_name="recetas")
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-fecha_creacion"]
+
+    def __str__(self):
+        return f" Comentario de {self.autor.username} en {self.receta.titulo}"
+    
+
+
