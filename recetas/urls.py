9c8ac406f174ae0b7b3c789d04841ec0248e535e
Juan Antonio Castro Ruiz <jcasrui733@fp.iesromerovargas.com>
2026-05-29 12:31:25 +0200

    cambios en base y en urls

--- recetas/urls.py
+++ recetas/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ListaRecetasView, DetalleRecetaView, CrearRecetaView
 
 urlpatterns = [
-    path("",ListaRecetasView.as_view(), name= "lista_receta"),
-    path("receta/<int:pk>",DetalleRecetaView.as_view(), name = "detalles_receta"),
+    path("",ListaRecetasView.as_view(), name= "lista_recetas"),
+    path("receta/<int:pk>/",DetalleRecetaView.as_view(), name = "detalles_receta"),
     path("crear/", CrearRecetaView.as_view(), name = "crear_receta")
 ]
